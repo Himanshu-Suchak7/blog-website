40879ba88f7affa9939cce2ed30792def720c804
Himanshu-Suchak7 <himanshusuchak.hs@gmail.com>
2024-06-19 12:24:36 +0530

    Improve blog delete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -167,6 +167,7 @@ const Home = ({ setActive, user, active }) => {
         setLoading(true);
         await deleteDoc(doc(db, "blogs", id));
         toast.success("Blog deleted successfully");
+        getBlogs();
         setLoading(false);
       } catch (err) {
         console.log(err);
